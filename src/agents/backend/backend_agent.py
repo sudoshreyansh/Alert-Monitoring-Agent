f8d0909f2fdc9784161e6a8a07dcb5daa78975d2
sudoshreyansh <shreyanshjain14072002@gmail.com>
2023-10-11 02:44:43 +0530

    updated backend agent

--- src/agents/backend/backend_agent.py
+++ src/agents/backend/backend_agent.py
@@ -5,8 +5,9 @@ from pydantic import BaseModel
 from agents.alerts.alert_agent import agent as alert_agent
 from agents.notifications.notification_agent import agent as notification_agent
 from agents.sources.source_agent import agent as source_agent
-from messages.alert import AlertRequest
-from utils.future import create_new_future
+from messages.alert import AlertRequest, AlertResponse
+from messages.notification import Notification
+from uagents.query import query
 
 ALERT_AGENT_ADDRESS = alert_agent.address
 NOTIFICATION_AGENT_ADDRESS = notification_agent.address
@@ -24,39 +25,44 @@ class QueueItem(BaseModel):
   phone_number: str
   max_temperature: float
   min_temperature: float
-  ack: asyncio.Future
 
 @app.get("/api/alert")
 async def add_alert(loc: str, phone: str, max_t: float, min_t: float):
-  future = create_new_future()
   request = QueueItem(
     location=loc,
     phone_number=phone,
     max_temperature=max_t,
     min_temperature=min_t,
-    ack=future
   )
 
   queue.put_nowait(request)
-  response = await request.ack
-
-  return response
+  return {'success': True}
 
 
-# @agent.on_interval(period=1.0)
-# async def handle_requests(ctx: Context):
-#   try:
-#     while True:
-#       request = queue.get_nowait()
-#       alert_request = AlertRequest(
-#         source=TEMPERATURE_SOURCE_AGENT_ADDRESS,
+@agent.on_interval(period=1.0)
+async def handle_requests(ctx: Context):
+  try:
+    while True:
+      request = queue.get_nowait()
+      envelope = await query(ALERT_AGENT_ADDRESS, AlertRequest(
+        source=TEMPERATURE_SOURCE_AGENT_ADDRESS,
+        field="main.temperature",
+        params={
+          "q": request.location
+        },
+        threshold=request.max_temperature
+      ))
+      alert = envelope.decode_payload().id
 
-#       )
+      ctx.storage.set(alert, request.phone_number)
+      queue.task_done()
+  except asyncio.QueueEmpty:
+    return
 
-#       queue.task_done()
-#   except asyncio.QueueEmpty:
-#     return
-
-
-# @agent.on_message(model, repl)
-# async def handle_alerts(ctx: Context, ):
+@agent.on_message(model=AlertResponse)
+async def handle_alerts(ctx: Context, sender: str, message: AlertResponse):
+  phone_number = ctx.storage.get(message.id)
+  await ctx.send(NOTIFICATION_AGENT_ADDRESS, Notification(
+    phone_number=phone_number,
+    message="The current temperature crossed the threshold temperature."
+  ))
